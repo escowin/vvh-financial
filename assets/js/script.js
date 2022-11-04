9a9981cb2fce7a3fb1d236c353d03016f9abf173
escowin <edwin@escowinart.com>
2022-11-04 18:12:38 -0500

    captured #cta-form data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,38 @@
-function copyright() {
-    let date = new Date().getFullYear();
+const ctaForm = document.getElementById("cta-form");
 
-    const copyrightEl = document.getElementById('copyright');
-    if (date >= 2022) {
-        copyrightEl.innerText = `\u00A9 ${date}`;
-    } else {
-        copyrightEl.innerText = `\u00A9 2022 - ${date}`;
-    }
+let prospects = [];
+
+// logic
+function copyright() {
+  let date = new Date().getFullYear();
+  console.log(`
+    \u00A9 ${date} Edwin M. Escobar
+    https://github.com/escowin
+    `);
+
+  const copyrightEl = document.getElementById("copyright");
+  if (date >= 2022) {
+    copyrightEl.innerText = `\u00A9 ${date}`;
+  } else {
+    copyrightEl.innerText = `\u00A9 2022 - ${date}`;
+  }
 };
 
+function formSubmit(e) {
+  e.preventDefault();
+  const firstNameInput = document.getElementById("first-name-input");
+  const lastNameInput = document.getElementById("last-name-input");
+  const phoneNumberInput = document.getElementById("phone-number-input");
+  const emailInput = document.getElementById("email-input");
+
+  prospect = {
+    firstName: firstNameInput.value,
+    lastName: lastNameInput.value,
+    phoneNumber: phoneNumberInput.value,
+    email: emailInput.value,
+  };
+};
+
+// calls
+ctaForm.addEventListener("submit", formSubmit);
 copyright();
