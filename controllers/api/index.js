d59579d5a42cce993a3ac773ede6e1aaa6ca7b62
escowin <edwin@escowinart.com>
2023-01-09 18:57:57 -0600

    set up | mvc scaffold

---
+++ controllers/api/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+// const agentRoutes = require('./agent-routes');
+// const clientRoutes = require('./client-routes');
+// const postRoutes = require('./post-routes');
+// const commentRoutes = require('./comment-routes');
+
+// api endpoints | /api/<route name>
+// router.use('/agent', agentRoutes);
+// router.use('/client', clientRoutes);
+// router.use('/posts', postRoutes);
+// router.use('/comments', commentRoutes);
+
+module.exports = router;
---
+++ models/index.js
@@ -0,0 +1,7 @@
+// models
+
+
+// associations
+// 1:M client : appointment
+// - can make many appointments
+// - each appointment belongs 
