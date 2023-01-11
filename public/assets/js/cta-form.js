17053bdef774fa8a048b55864dad6802b4fab33d
escowin <edwin@escowinart.com>
2023-01-10 23:56:34 -0600

    homepage | cta-form | captured values

--- public/assets/js/cta-form.js
+++ public/assets/js/cta-form.js
@@ -1,7 +1,19 @@
 async function ctaFormHandler(e) {
     e.preventDefault();
 
-    console.log("clicked")
+    const firstName = document.getElementById('first-name').value.trim();
+    const lastName = document.getElementById('last-name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const phone = document.getElementById('phone').value.trim();
+    const contact = document.querySelector('input[name="contact"]').value.trim();
+    const clientText = document.getElementById('client-text').value.trim();
+
+    console.log(firstName)
+    console.log(lastName)
+    console.log(email)
+    console.log(phone)
+    console.log(contact)
+    console.log(clientText)
 };
 
 document.getElementById('cta-form').addEventListener('submit', ctaFormHandler);
