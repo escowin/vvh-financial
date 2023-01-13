254be73a263417ac88e248eadbd4dd67043d24c5
escowin <edwin@escowinart.com>
2023-01-12 20:24:28 -0600

    styles | homepage, login, and dashboard views

--- public/javascript/copyright.js
+++ public/javascript/copyright.js
@@ -1,5 +1,9 @@
 function copyright() {
     let date = new Date().getFullYear();
+    console.log(`\u00a9 ${date} VVH Financial 
+    built by Edwin M. Escobar
+    https://github.com/escowin/vvh-financial`);
+
     const copyrightEl = document.getElementById('copyright');
     copyrightEl.innerText = date;
 }
